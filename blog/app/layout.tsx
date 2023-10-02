8537ec706fbc9d17f968997b4770405c4031e02d
Yusuff Atanda <Yvsvfff@gmail.com>
2023-10-01 22:33:51 -0400

    update

---
+++ blog/app/layout.tsx
@@ -0,0 +1,30 @@
+
+import Header from '@components/Header'
+import './globals.css'
+import type { Metadata } from 'next'
+import Banner from '@components/Banner'
+
+
+
+
+
+export const metadata: Metadata = {
+  
+  title: 'Yvsvff Blog',
+  description: 'Created by Yusuff Atanda',
+ 
+}
+
+
+
+export default async function RootLayout({children}: {children: React.ReactNode}) {
+  return (
+    <html lang="en">
+      <body className='max-w-7xl mx-auto'>
+        <Header />
+        <Banner />
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ blog/app/page.tsx
@@ -0,0 +1,26 @@
+
+import BlogList from "@components/BlogList";
+import { client } from "@sanity/lib/client"
+import { groq } from "next-sanity"
+
+
+const query = groq`
+*[_type=='post'] {
+  ...,
+  author->,
+  categories[]->
+} | order(_createdAt desc)`
+
+export default async function HomePage() {
+
+  const posts = await client.fetch(query);
+ 
+  
+
+  return <BlogList posts={posts} />
+  console.log(posts)
+     
+      
+  
+}
+
