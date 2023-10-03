190a1367475bd4e9b27906f1ad594fd3a1a745f8
Yusuff Atanda <Yvsvfff@gmail.com>
2023-10-03 16:05:33 -0400

    update

--- blog/app/page.tsx
+++ blog/app/page.tsx
@@ -4,23 +4,24 @@ import { client } from "@sanity/lib/client"
 import { groq } from "next-sanity"
 
 
+
 const query = groq`
-*[_type=='post'] {
+*[_type=='post']{
   ...,
   author->,
-  categories[]->
-} | order(_createdAt desc)`
+  categories[]->,
+}|order(_createdAt desc)`
+
+export const revalidate = 30;
 
 export default async function HomePage() {
-
-  const posts = await client.fetch(query);
- 
-  
-
-  return <BlogList posts={posts} />
-  console.log(posts)
-     
-      
-  
+  try {
+    const posts = await client.fetch(query);
+    return <BlogList posts={posts} />;
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    // Optionally, you can render an error message or handle the error in another way.
+  }
 }
+
 
--- blog/sanity/lib/image.ts
+++ blog/sanity/lib/image.ts
@@ -4,11 +4,12 @@ import createImageUrlBuilder from '@sanity/image-url'
 import { dataset, projectId } from '../env'
 
 
+
 const imageBuilder = createImageUrlBuilder({
   projectId: projectId || '',
   dataset: dataset || '',
 });
 
-export const urlFor = (source: any) => {
+export const urlFor = (source: Image) => {
   return imageBuilder?.image(source).auto('format').fit('max')
 }
